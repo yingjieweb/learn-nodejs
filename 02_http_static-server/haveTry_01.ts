4cc252c0358facc8b75a5684a15aab66a74be734
yingjieweb <1184061078@qq.com>
2023-02-25 13:19:25 +0800

    feat: 🎸 return resources base on different url

---
+++ 02_http_static-server/haveTry_01.ts
@@ -0,0 +1,48 @@
+import * as http from "http";
+import { IncomingMessage, ServerResponse } from "http";
+import * as fs from "fs";
+import * as path from "path";
+
+const server = http.createServer();
+
+server.on("request", (request: IncomingMessage, response: ServerResponse) => {
+  const { method, url, headers } = request;
+  console.log(method, url, headers);
+
+  switch (url) {
+    case "/index.html":
+      response.setHeader("Content-Type", "text/html; charset=utf-8");
+      fs.readFile(
+        path.resolve(__dirname, "public", "index.html"),
+        (error, data) => {
+          if (error) throw error;
+          response.end(data);
+        }
+      );
+      break;
+    case "/style.css":
+      response.setHeader("Content-Type", "text/css; charset=utf-8");
+      fs.readFile(
+        path.resolve(__dirname, "public", "style.css"),
+        (error, data) => {
+          if (error) throw error;
+          response.end(data);
+        }
+      );
+      break;
+    case "/main.js":
+      response.setHeader("Content-Type", "text/javascript; charset=utf-8");
+      fs.readFile(
+        path.resolve(__dirname, "public", "main.js"),
+        (error, data) => {
+          if (error) throw error;
+          response.end(data);
+        }
+      );
+      break;
+    default:
+      break;
+  }
+});
+
+server.listen(8888);
